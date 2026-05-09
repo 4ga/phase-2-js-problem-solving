f855b5b27108c6c158cf5e40f110d009db66c94a
Grace A <gakpgit@gmail.com>
2026-05-09 17:04:02 -0600

    Add min heap extract min and sink down

--- src/MinHeap.js
+++ src/MinHeap.js
@@ -2,6 +2,51 @@ class MinHeap {
  constructor() {
   this.values = [];
  }
+ extractMin() {
+  if(this.values.length === 0) {
+   return undefined;
+  }
+  if(this.values.length === 1) {
+   return this.values.pop();
+  }
+  const min = this.values[0];
+  const last = this.values.pop();
+
+  this.values[0] = last;
+  this.sinkDown();
+
+  return min;
+ }
+
+ sinkDown() {
+  let index = 0;
+  const length = this.values.length;
+
+  while(true) {
+   const leftChildIndex = 2 * index + 1;
+   const rightChildIndex = 2 * index + 2;
+   let smallestIndex = index;
+
+   if(leftChildIndex < length && this.values[leftChildIndex] < this.values[smallestIndex]){
+    smallestIndex = leftChildIndex;
+   }
+   
+   if(rightChildIndex < length && this.values[rightChildIndex] < this.values[smallestIndex]){
+    smallestIndex = rightChildIndex;
+   }
+   
+   if(smallestIndex === index) {
+    break;
+   }
+  
+   const temp = this.values[index];
+   this.values[index] = this.values[smallestIndex];
+   this.values[smallestIndex] = temp;
+
+   index = smallestIndex;
+  }
+ }
+
  peek() {
   return this.values[0];
  }
--- tests/minHeap.test.js
+++ tests/minHeap.test.js
@@ -55,4 +55,60 @@ describe("MinHeap tests", () => {
   expect(heap.peek()).toBe(5);
   expect(heap.values[0]).toBe(5);
  });
+
+ it("extractMin returns undefined for an empty heap", () => {
+  const heap = new MinHeap();
+
+  expect(heap.extractMin()).toBeUndefined();
+ });
+
+ it("extractMin removes and returns the only value in the heap", () => {
+  const heap = new MinHeap();
+  
+  heap.insert(10);
+
+  expect(heap.extractMin()).toBe(10);
+  expect(heap.values).toEqual([]);
+ });
+
+ it("extractMin removes the smallest value from a heap with two values", () => {
+  const heap = new MinHeap();
+
+  heap.insert(10);
+  heap.insert(5);
+
+  expect(heap.extractMin()).toBe(5);
+  expect(heap.values).toEqual([10]);
+  expect(heap.peek()).toBe(10);
+ });
+
+ it("extractMin sinks the new root down to restore the heap property", () => {
+  const heap = new MinHeap();
+
+  heap.insert(3);
+  heap.insert(8);
+  heap.insert(5);
+  heap.insert(20);
+  heap.insert(10);
+  
+  expect(heap.extractMin()).toBe(3);
+  expect(heap.peek()).toBe(5);
+ });
+
+ it("extractMin repeatedly removes values from smallest to largest", () => {
+  const heap = new MinHeap();
+ 
+  heap.insert(20);
+  heap.insert(5);
+  heap.insert(15);
+  heap.insert(3);
+  heap.insert(10);
+
+  expect(heap.extractMin()).toBe(3);
+  expect(heap.extractMin()).toBe(5);
+  expect(heap.extractMin()).toBe(10);
+  expect(heap.extractMin()).toBe(15);
+  expect(heap.extractMin()).toBe(20);
+  expect(heap.extractMin()).toBeUndefined();
+ });
 });
